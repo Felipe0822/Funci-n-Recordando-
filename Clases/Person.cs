012380c6ff9b10a332e46dadc0fc8b82c26d772f
Felipe <feliperincon77@gmail.com>
2024-08-17 16:27:10 -0500

    faltaFuncion

---
+++ Clases/Person.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_2
+{
+    internal class Person
+    {
+        private int idPerson;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private int phoneNumber;
+        private string email;
+        private int height;
+        private string gender;
+
+        public Person() { }
+        public Person(int cedula, string nombre, string apellido)
+        {
+            this.idPerson = cedula;
+            this.firstName = nombre;
+            this.lastName = apellido;
+        }
+
+        public Person(int idPerson, string firstName, string lastName, int age, int phoneNumber, string email, int height, string gender)
+        {
+            this.idPerson = idPerson;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.height = height;
+            this.gender = gender;
+        }
+
+        public Person(string email)
+        {
+            this.email = email;
+        }
+
+        //Encapsulamiento
+        public int IdPerson { get => idPerson; set => idPerson = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Age { get => age; set => age = value; }
+        public int PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Email { get => email; set => email = value; }
+        public int Height { get => height; set => height = value; }
+        public string Gender { get => gender; set => gender = value; }
+
+        #region Crear metodos 
+        public void ViewData()
+        {
+            Console.WriteLine("┌────────────────────────────┐");
+            Console.WriteLine("│ ** Datos de la persona **  │");
+            Console.WriteLine("└────────────────────────────┘");
+            Console.WriteLine($"\tIdentificacion -> {idPerson}");
+            Console.WriteLine($"\tNombre -> {firstName}");
+            Console.WriteLine($"\tApellido -> {lastName}");
+            Console.WriteLine($"\tEdad -> {age}");
+            Console.WriteLine($"\tEstatura -> {height}");
+            Console.WriteLine($"\tGenero -> {gender}");
+            Console.WriteLine($"\tCelular -> {phoneNumber}");
+            Console.WriteLine($"\tCorreo -> {email}");
+            Console.WriteLine("└───────────────────────────────┘");
+
+        }
+        public void UpdateGender(string genero)
+        {
+            this.gender = genero;
+        }
+        public void UpdateFirstName(string nombre)
+        {
+            this.firstName = nombre;
+        }
+        public void UpdateLastName(string apellido)
+        {
+            this.lastName = apellido;
+        }
+        public void UpdateAge(int edad)
+        {
+            this.age = edad;
+        }
+        public void UpdateHeight(int altura)
+        {
+            this.Height = altura;
+        }
+        public void UpdatePhone(int numero)
+        {
+            this.phoneNumber = numero;
+        }
+        public void UpdateEmail(string correo)
+        {
+            this.email = correo;
+        }
+        public void UpdateId(int id)
+        {
+            this.idPerson = id;
+        }
+       
+                
+
+
+
+
+        #endregion
+    }
+}
+
---
+++ Clases/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> emails = new List<string>();
+            int eleccion = 0;
+            int actu = 0;
+
+            Person person1 = new Person(1234, "Pipe", "Rincon", 21, 1312442421, "pipe@gmail.com", 176, "Male");
+            
+                Console.WriteLine("Desea actulizar datos\n1.Si, 2.No");
+                string accion = Console.ReadLine();
+                if (int.TryParse(accion, out eleccion))
+                {
+                    while (eleccion == 1)
+                    {
+                        person1.ViewData();
+                        Console.WriteLine("Que desea actualizar? 1:ID, 2:Nombre, 3:Apellido, \n4:Genero, 5:Edad, 6:Numero de telefono," +
+                            " 7:Reemplazar Email principal, 8:Agregar Email, 9:Altura");
+                        actu = int.Parse(Console.ReadLine());
+                        if (actu == 1)
+                        {
+                            Console.WriteLine("Ingrese la nueva ID");
+                            person1.UpdateId(int.Parse(Console.ReadLine()));
+                            person1.ViewData();
+                        }
+                        else if (actu == 2)
+                        {
+                            Console.WriteLine("Ingrese el nuevo nombre");
+                            person1.UpdateFirstName(Console.ReadLine());
+                            person1.ViewData();
+                        }
+                        else if (actu == 3)
+                        {
+                            Console.WriteLine("Ingrese el nuevo apellido");
+                            person1.UpdateLastName(Console.ReadLine());
+                            person1.ViewData();
+                        }
+                        else if (actu == 4)
+                        {
+                            Console.WriteLine("Ingrese el nuevo genero");
+                            person1.UpdateGender(Console.ReadLine());
+                            person1.ViewData();
+                        }
+                        else if (actu == 5)
+                        {
+                            Console.WriteLine("Ingrese la nueva edad");
+                            person1.UpdateAge(int.Parse(Console.ReadLine()));
+                            person1.ViewData();
+                        }
+                        else if (actu == 6)
+                        {
+                            Console.WriteLine("Ingrese el nuevo numero de telefono");
+                            person1.UpdatePhone(int.Parse(Console.ReadLine()));
+                            person1.ViewData();
+                        }
+                        else if (actu == 7)
+                        {
+                            Console.WriteLine("Ingrese el nuevo email principal, este eliminara el anterior");
+                            person1.UpdateEmail(Console.ReadLine());
+                            person1.ViewData();
+                        }
+                        else if (actu == 8)
+                        {
+                            Console.WriteLine("Ingrese el nuevo email");
+                            emails.Add(Console.ReadLine());
+                            Console.WriteLine("Tiene los siguientes correos añadidos:");
+                            foreach (string mails in emails)
+                            {
+                                Console.WriteLine(mails);
+                            }
+
+                        }
+                        else if (actu == 9)
+                        {
+                            Console.WriteLine("Ingrese la nueva altura");
+                            person1.UpdateHeight(int.Parse(Console.ReadLine()));
+                            person1.ViewData();
+                        }
+                        Console.WriteLine("Desea actualizar datos\n1.Si, 2.No");
+                        eleccion = int.Parse(Console.ReadLine());
+                    }
+                }
+            
+           
+        }
+    }
+}
+
